22070df8c21738e495547095d67e304619be1203
Borna <borna@borna-almasi.com>
2014-02-19 16:23:23 -0500

    Initial import

---
+++ app/scripts/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+angular.module('youtubeApiApp', [
+  'ngResource',
+  'ngRoute'
+])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });

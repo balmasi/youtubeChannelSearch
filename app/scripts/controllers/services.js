8a422ca085f730a64fd3a158598e4224197927a3
Borna <borna@borna-almasi.com>
2014-02-19 19:11:32 -0500

    Added firebase and channel services

--- app/scripts/controllers/services.js
+++ app/scripts/controllers/services.js
@@ -3,3 +3,36 @@
  */
 
 
+/**
+ * Created by balmasi on 19/02/14.
+ */
+'use strict';
+
+angular.module('youtubeApiApp.services', [])
+  .factory('FireRef', ['FBURL', 'Firebase',
+    function(FBURL, Firebase) {
+      return {
+        channels: function() {
+          return new Firebase(FBURL+'/channels');
+        }
+      };
+    }]
+  )
+  .factory('channelService', ['angularFireCollection', 'FireRef',
+    function(angularFireCollection, FireRef) {
+      return {
+        collection: function(cb) {
+          return angularFireCollection(FireRef.channels(),cb);
+        }
+        , find: function(channelId) {
+          return FireRef.channels().child('/'+channelId);
+        }
+        , create: function(channel) {
+          return FireRef.leagues().push(channel);
+        }
+        , removeChannel: function(channelId) {
+          var channel = FireRef.channels().child('/'+channelId);
+          channel.remove();
+        }
+      };
+    }]);

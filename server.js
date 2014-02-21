9146f11c14881636afa5a2bf761a76a05c44230b
Borna <borna@borna-almasi.com>
2014-02-21 16:30:46 -0500

    Added files for heroku deploy

---
+++ server.js
@@ -0,0 +1,11 @@
+/**
+ * Created by balmasi on 21/02/14.
+ */
+
+var gzippo = require('gzippo');
+var express = require('express');
+var app = express();
+
+app.use(express.logger('dev'));
+app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+app.listen(process.env.PORT || 5000);
